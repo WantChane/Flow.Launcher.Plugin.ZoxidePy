98dca9af9efb39fef8d5bb464f2644f0cc73f495
WantChane <wantchane@gmail.com>
2025-07-15 09:56:28 +0800

    chore(imports): remove unused and redundant imports

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import logging
 from pyflowlauncher import Plugin, Result, send_results
 from pyflowlauncher.result import ResultResponse
 from pyflowlauncher.api import copy_to_clipboard
-from pyflowlauncher.icons import FOLDER, COPY, RECYCLEBIN, ERROR
+from pyflowlauncher.icons import FOLDER, COPY, RECYCLEBIN
 from error import *
 
 plugindir = Path.absolute(Path(__file__).parent)
